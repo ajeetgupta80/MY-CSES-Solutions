549baf02fad7c3aa5335295ea0e41dbc3146060e
fucking ajeet <ajeet808080@gmail.com>
2023-09-20 11:44:07 +0530

    Graph

---
+++ CountingRoom.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+void init_code(){
+    #ifndef ONLINE_JUDGE
+    freopen("input_1.txt", "r", stdin);
+    freopen("output_1.txt", "w", stdout);
+    #endif 
+}
+
+
+
+
+
+
+int main(){
+	init_code();
+	cout<<10<<endl;
+
+}
