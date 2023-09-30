ca769a6dacd00f147a39ecf1757486e9146b69ca
fucking ajeet <ajeet808080@gmail.com>
2023-09-30 09:36:27 +0530

    Graph

--- CountingRoom.cpp
+++ CountingRoom.cpp
@@ -12,12 +12,54 @@ void init_code(){
 }
 
 
+void dfs(int i,int j, vector<vector<int>> &vis, vector<vector<char>> &grid, int dr[], int dx[], int n, int m){
+    vis[i][j] = 1;
+
+    for(int k=0; k<4; ++k){
+        int nrow = i+dr[k];
+        int ncol = j+dx[k];
+      if(nrow>=0 && ncol>=0 && nrow<n && ncol<m){
+        if(!vis[nrow][ncol] && grid[nrow][ncol] == '.'){
+            dfs(nrow, ncol , vis, grid, dr , dx, n,m);
+        }
+    }
+}
+
+
+
+}
 
 
 
 
 int main(){
 	init_code();
-	cout<<10<<endl;
+    int n, m;
+    cin >>n >> m;
+	vector<vector<char>> grid(n);
+    for(int i =0; i<n; ++i){
+        for(int j = 0; j<m; ++j){
+             char x;
+             cin >> x;
+             grid[i].push_back(x);
+        }
+    }
 
+    
+    int dr[] = {-1, 0, 1, 0};
+    int dx[] = {0, 1 , 0 ,-1};
+    int count = 0;
+    vector<vector<int>> vis(n, vector<int> (m , 0));
+    for(int i =0; i<n; ++i){
+        for(int j=0; j<m; ++j){
+
+            if(vis[i][j]==0 && grid[i][j] =='.'){
+                count++;
+                dfs(i, j , vis, grid, dr, dx, n, m);
+
+            }
+        }
+    }
+    cout<<count<<endl; 
+    
 }
